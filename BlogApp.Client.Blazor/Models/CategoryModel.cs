fa8326e6314e2b011ed0996e7d66b2e83cb07daf
mehmethamzakadi <Mehmet17>
2024-12-18 21:28:49 +0300

    kategori işlemleri düzenlemeleri.

--- BlogApp.Client.Blazor/Models/CategoryModel.cs
+++ BlogApp.Client.Blazor/Models/CategoryModel.cs
@@ -7,7 +7,7 @@ public class CategoryModel
     public int Id { get; set; }
 
     [Required(ErrorMessage = "Kategori adı zorunludur.")]
-    [MinLength(2, ErrorMessage = "Kategori adı en az 2 karakter olmalıdır.")]
-    [MaxLength(50, ErrorMessage = "Kategori adı en fazla 50 karakter olmalıdır.")]
+    [MinLength(5, ErrorMessage = "Kategori adı en az 5 karakter olmalıdır.")]
+    [MaxLength(100, ErrorMessage = "Kategori adı en fazla 100 karakter olmalıdır.")]
     public string Name { get; set; }
 }
